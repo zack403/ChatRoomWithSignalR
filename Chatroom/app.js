4aebd42bf9ae91eb29bd16870c285ea9b2f5c3c8
zack <aminuzack7@gmail.com>
2018-04-09 00:22:38 +0100

    first commit

---
+++ Chatroom/app.js
@@ -0,0 +1,38 @@
+﻿//var name = "";
+angular.module('chat', ['SignalR', 'ngRoute'])
+    // Handles the events where users log in and log out
+    .controller('Login', ['signalRService', '$rootScope', function (signalRService, $rootScope) {
+        this.loggedIn = false;
+        this.rootScope = $rootScope;
+        this.rootScope.name = "";
+        this.signalRService = signalRService;
+        var that = this;
+        this.logIn = function () {
+            var element = document.getElementById('name');
+            var name = element.value;
+            if (name !== "") {
+                signalRService.goOnline(name); // allows all clients to see you went online
+                signalRService.broadcastMessage("server", "*" + name + " has entered the room*");
+                that.rootScope.name = name;
+                that.loggedIn = true;
+                element.value = "";
+            }
+        };
+        this.logOut = function () {
+            that.loggedIn = false;
+            signalRService.goOffline(that.rootScope.name); // allows all clients to see you went offline
+            signalRService.broadcastMessage("server", "*" + that.rootScope.name + " has left the room*");
+        }
+    }])
+    // Handles the event where user enter a message in the chat room
+    .controller('EnterMessage', ['signalRService', '$rootScope', function (signalRService, $rootScope) {
+        this.message = "";
+        this.enterText = function () {
+            var text = document.getElementById('text');
+            // only broadcast if text is not empty
+            if (text.value !== "") {
+                signalRService.broadcastMessage($rootScope.name, text.value); // broadcast that message to all other Clients
+                text.value = "";
+            }
+        };
+    }]);
---
+++ Chatroom/hub/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Chatroom
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+
+            if (name == "server")
+            {
+                Clients.All.enter(name, message);
+                Clients.All.setColor("black");
+            }
+            else
+            {
+                Clients.Caller.enter("you", message);
+                Clients.Caller.setColor("blue");
+
+                Clients.Others.enter(name, message);
+                Clients.Others.setColor("red");
+
+            }
+        }
+        public void SetOnline(string name)
+        {
+            // appear online to all connected clients
+            Clients.All.setOnline(name);
+        }
+        public void SetOffline(string name)
+        {
+            // appear offline to all connected clients
+            Clients.All.setOffline(name);
+        }
+    
+    }
+}
